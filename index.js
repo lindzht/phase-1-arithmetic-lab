98226ceb083c08ed61e02ea59bb6021ea42d2b33
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-10-07 19:25:04 -0400

    commit tree abc0c63df62be8161fb1019505e66afd6319530f

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random())+ 1;
+
+const num3 = 22
+const num4 = 18
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 20);
